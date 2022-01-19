f8b877e02b9eebeb4a0a80a51480aab5462dbbca
tms0052 <tms0052@mix.wvu.edu>
2022-01-18 20:07:53 -0500

    Added topnavbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TopNavBarComponent } from 'src/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule
